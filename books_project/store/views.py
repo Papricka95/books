6f3b9803ff2a9586f0cd6bf4e06555258b4a21f2
Papricka95 <wadim.karlov@mail.ru>
2021-12-14 23:49:44 +0400

    create: filters, searching, ordering and test to them

--- books_project/store/views.py
+++ books_project/store/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 from store.models import Book
 from store.serializer import BookSerializer
@@ -6,3 +8,12 @@ from store.serializer import BookSerializer
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    # filter
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_fields = ['price']
+    # search
+    search_fields = ['name', 'author_name']
+
+    #order
+    ordering_fields = ['price', 'author_name']
